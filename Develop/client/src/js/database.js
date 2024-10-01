4aa66116a0146c63c66b699d595dad9e65c1457b
ssoto83 <168875470+ssoto83@users.noreply.github.com>
2024-09-30 23:31:07 -0400

    added the missing logic in databse.js and install.js from the client directory

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,18 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('adding content to the database');
+  const db = await openDB('jate', 1);
+  await db.add('jate', {content });
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('fetching content from the database');
+  const db = await openDB('jate', 1);
+  const allContent = await db.getAll('jate');
+  return allContent;
+};
 
 initdb();
